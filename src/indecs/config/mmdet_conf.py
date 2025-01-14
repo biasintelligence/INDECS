99c6277023a457f1ce9118bd9a0fb1e2e95cf5c9
Ilya Buzytsky <ilya@biasintelligence.com>
2025-01-14 14:29:31 -0800

    Add .idea to .gitignore

--- src/indecs/config/mmdet_conf.py
+++ src/indecs/config/mmdet_conf.py
@@ -1,7 +1,5 @@
 import mmcv
 import mmdet
-import mmengine
 
 print(f"MMCV version: {mmcv.__version__}")
 print(f"MMDet version: {mmdet.__version__}")
-print(f"MMEngine version: {mmengine.__version__}")
